9c353fee519f8f9830f1780d3b4c542d4efe6b74
hoomanzolfaghari <hoomanzolfaghari84@gmail.com>
2025-10-12 22:36:15 +0330

    Built the project with new submodules.

--- Sisyphus/src/Sisyphus/Core/EntryPoint.h
+++ Sisyphus/src/Sisyphus/Core/EntryPoint.h
@@ -12,15 +12,15 @@ int main(int argc, char** argv)
 {
 	Sisyphus::Log::Init();
 
-	HS_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Startup.json");
+	HS_PROFILE_BEGIN_SESSION("Startup", "SisyphusProfile-Startup.json");
 	auto app = Sisyphus::CreateApplication({ argc, argv });
 	HS_PROFILE_END_SESSION();
 
-	HS_PROFILE_BEGIN_SESSION("Runtime", "HazelProfile-Runtime.json");
+	HS_PROFILE_BEGIN_SESSION("Runtime", "SisyphusProfile-Runtime.json");
 	app->Run();
 	HS_PROFILE_END_SESSION();
 
-	HS_PROFILE_BEGIN_SESSION("Shutdown", "HazelProfile-Shutdown.json");
+	HS_PROFILE_BEGIN_SESSION("Shutdown", "SisyphusProfile-Shutdown.json");
 	delete app;
 	HS_PROFILE_END_SESSION();
 }
